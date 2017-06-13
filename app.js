ebf64381cb76ecfd493b09b106cc79d67641e74b
Scew5145 <scew5145@colorado.edu>
2017-06-13 17:10:56 -0600

    Finally got stuff working, time to break things again

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-/*var gcloud = require('google-cloud')({
-  projectId: 'videoconvertgcdemo'
-});*/
+const express = require('express');
 const functions = require('firebase-functions');
 const mkdirp = require('mkdirp-promise');
 const gcs = require('@google-cloud/storage')();
@@ -9,5 +7,21 @@ const ffmpeg = require('fluent-ffmpeg');
 const ffmpeg_static = require('ffmpeg-static');
 
 
-
+const app = express();
 console.log('Cloud Server Started! Probably.')
+
+app.get('/', (req, res) => {
+    res.status(200).send("These are some words that I'm sending to the user.")
+});
+
+
+if (module === require.main) {
+  // [START server]
+  // Start the server
+  const server = app.listen(process.env.PORT || 8081, () => {
+    const port = server.address().port;
+    console.log(`App listening on port ${port}`);
+    console.log(server.address())
+  });
+  // [END server]
+}
