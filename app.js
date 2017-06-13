6c6fe2b947688380d30696af8f3ca96e441fb7a5
Scew5145 <scew5145@colorado.edu>
2017-06-13 16:31:32 -0600

    Sign in  for cloud functions

--- app.js
+++ app.js
@@ -5,4 +5,8 @@ const Promise = require('bluebird');
 const ffmpeg = require('fluent-ffmpeg');
 const ffmpeg_static = require('ffmpeg-static');
 
+var gcloud = require('google-cloud')({
+  projectId: 'videoconvertgcdemo'
+});
+
 console.log('Cloud Server Started! Probably.')
