bb10cb8e2ef3092dc76f06428c8ce6d2d3827ea6
Scew5145 <scew5145@colorado.edu>
2017-06-13 15:56:54 -0600

    More setup

---
+++ app.js
@@ -0,0 +1,8 @@
+const functions = require('firebase-functions');
+const mkdirp = require('mkdirp-promise');
+const gcs = require('@google-cloud/storage')();
+const Promise = require('bluebird');
+const ffmpeg = require('fluent-ffmpeg');
+const ffmpeg_static = require('ffmpeg-static');
+
+console.log('Cloud Server Started! Probably.')
