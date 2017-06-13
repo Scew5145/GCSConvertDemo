4fee67663d8adf006deabd30820ed6eca4af24ad
Scew5145 <scew5145@colorado.edu>
2017-06-13 16:40:17 -0600

    More random shit

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+var gcloud = require('google-cloud')({
+  projectId: 'videoconvertgcdemo'
+});
+
 const functions = require('firebase-functions');
 const mkdirp = require('mkdirp-promise');
 const gcs = require('@google-cloud/storage')();
@@ -5,8 +9,6 @@ const Promise = require('bluebird');
 const ffmpeg = require('fluent-ffmpeg');
 const ffmpeg_static = require('ffmpeg-static');
 
-var gcloud = require('google-cloud')({
-  projectId: 'videoconvertgcdemo'
-});
+
 
 console.log('Cloud Server Started! Probably.')
